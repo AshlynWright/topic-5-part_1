f7087252d1323dcff9a54f0017534feac69989c3
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-03 09:16:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace topic_5_part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ashlyn Wright
+            //part 1 topic 5
+            //part 1
+
+                  double weight = 128;
+
+                  Console.WriteLine("Please enter your current weight:");
+                  double W = Convert.ToDouble(Console.ReadLine());  
+                  Console.WriteLine("I have information for the following planets:");
+                  Console.WriteLine("1. Venus 2. Mars 3. Jupiter. ");
+                  Console.WriteLine("4 Saturn 5. Uranus 6. Neptune");
+                  Console.WriteLine("Which planet are you visiting?");
+                  int p = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
