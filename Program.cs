fb53d3cd00cb622552af2afa763ff51cb48817c3
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-06 09:05:04 -0400

    don't start until after tut

--- Program.cs
+++ Program.cs
@@ -5,52 +5,6 @@
         static void Main(string[] args)
         {
 
-            int people = 20;
-            int cats = 20;
-            int dogs = 15;
-            Console.WriteLine("People: " + people + " Dogs: " + dogs + " Cats: " + cats);
-            if (people < cats)
-            {
-                Console.WriteLine("Too many cats! The world is doomed!");
-            }
-            if (people > cats)
-            {
-                Console.WriteLine("Not many cats! The world is saved!");
-            }
-            if (people < dogs)
-            {
-                Console.WriteLine("The world is drooled on!");
-            }
-            if (people > dogs)
-            {
-                Console.WriteLine("The world is dry!");
-            }
-            Console.WriteLine("Press ENTER to continue. ");
-            Console.ReadLine();
-            Console.Clear();
-            dogs += 5; // Add 5 to dogs. What does dogs equal now?
-            Console.WriteLine("People: " + people + " Dogs: " + dogs + " Cats: " + cats);
-            if (people >= dogs)
-            {
-                Console.WriteLine("People are greater than or equal to dogs.");
-            }
-            if (people <= dogs)
-            {
-                Console.WriteLine("People are less than or equal to dogs.");
-            }
-            if (people == dogs)
-            {
-                Console.WriteLine("People are dogs.");
-            }
-
-
-
-
-
-
-
-
-
             //Ashlyn Wright
             //part 1 topic 5
             //part 1
