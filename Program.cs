a42db51c78aaebc1acacbf7e7559aed3a81d2449
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-27 09:02:18 -0400

    Almost done

--- Program.cs
+++ Program.cs
@@ -9,39 +9,31 @@
             //part 1 topic 5
             //part 1
 
-            //      double weight = 128;
+            double earthweight = 128;
 
-            //      string planets;
-            //      double gravity;
+            string planets;
+            double gravity;
 
-            //      double pounds;
+            double pounds;
 
-            //      string venus;
-            //      string mars;
-            //      double jupiter = 2.65;
-            //      double saturn = 1.17;
-            //      double uranus = 1.05;
-            //      double neptune = 1.23;
-            //      double pounds;
-
+            double venus = 0.78;
+            double mars = 0.39;
+            double jupiter = 2.65;
+            double saturn = 1.17;
+            double uranus = 1.05;
+            double neptune = 1.23;
 
 
+            Console.WriteLine("Please enter your current earth weight:");
+            double earthWeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("I have information for the following planets:");
+            Console.WriteLine("1. Venus 2. Mars 3. Jupiter. ");
+            Console.WriteLine("4 Saturn 5. Uranus 6. Neptune");
+            Console.WriteLine("Which planet are you visiting?");
+            double planet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your weight would be + (earthweight * planet");
 
-
-
-
-
-
-
-            //Console.WriteLine("Please enter your current earth weight:");
-            //      double Weight = Convert.ToDouble(Console.ReadLine());  
-            //      Console.WriteLine("I have information for the following planets:");
-            //      Console.WriteLine("1. Venus 2. Mars 3. Jupiter. ");
-            //      Console.WriteLine("4 Saturn 5. Uranus 6. Neptune");
-            //      Console.WriteLine("Which planet are you visiting?");
-            //      double p = Convert.ToInt32(Console.ReadLine());
-            //      Console.WriteLine("your weight would be:" +  weight * pounds);
-            //      Console.ReadLine();
+            
 
 
 
